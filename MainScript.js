43b17d16db71d71b8ac2bae2bceb0532e21f1fb3
TruongBradley <150624922+TruongBradley@users.noreply.github.com>
2023-12-03 22:02:36 -0500

    Add files via upload

---
+++ MainScript.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("myMenu").style.marginLeft = "15%";
+  document.getElementById("myMain").style.marginLeft = "15%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("myMenu").style.marginLeft = "0";
+  document.getElementById("myMain").style.marginLeft = "0%";
+}
