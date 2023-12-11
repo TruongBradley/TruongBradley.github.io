dec79e127b3e5d689b5e022f8764620340a4b67f
TruongBradley <150624922+TruongBradley@users.noreply.github.com>
2023-12-10 21:08:26 -0500

    Add files via upload
    Updated Copy for Unit 5 final touches

--- MainScript.js
+++ MainScript.js
@@ -1,5 +1,19 @@
-/* Set the width of the side navigation to 250px */
+var menuStatusOpen = false
+
+function menuButtonToggle() {
+  switch (menuStatusOpen) {
+  case false:
+    openNav()
+    break;
+  case true:
+    closeNav()
+    break;
+  }
+}
+
+/* Set the width of the side navigation to 15 percent */
 function openNav() {
+  menuStatusOpen = true
   document.getElementById("mySidenav").style.width = "15%";
   document.getElementById("myMenu").style.marginLeft = "15%";
   document.getElementById("myMain").style.marginLeft = "15%";
@@ -7,6 +21,7 @@ function openNav() {
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
+  menuStatusOpen = false
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("myMenu").style.marginLeft = "0";
   document.getElementById("myMain").style.marginLeft = "0%";
